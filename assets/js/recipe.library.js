7fa85e54f5805c634277310524cc4a70ea75b376
Hunter <hunterwest94@rocketmail.com>
2025-01-16 22:19:13 -0500

    commit css on several elements in recipe library

--- assets/js/recipe.library.js
+++ assets/js/recipe.library.js
@@ -14,15 +14,21 @@ parsedRecipe.forEach((recipe, index) => {
     const divItem = document.createElement('div');
     divItem.classList.add('container');
     divItem.classList.add('box');
+
 //Created element named recipeName 
     const recipeName = document.createElement('p');
     recipeName.classList.add('title');
     recipeName.classList.add('recipeLibraryName');
     recipeName.innerHTML = `${recipe.name}`;
+
+    const recipePic = atob(parsedRecipe, 'photo');
+    
+
 //Seperate ingredient section
     const ingSection = document.createElement('h2');
     ingSection.classList.add('recipeLibraryIngredients');
     ingSection.innerHTML = 'Ingredients';
+
 //Created element named ingredientsList
     const ingredientsList = document.createElement('ul');
     ingredientsList.classList.add('recipeLibraryIngredients');
@@ -34,24 +40,32 @@ parsedRecipe.forEach((recipe, index) => {
     ingitem.innerHTML = ingredient;
     ingredientsList.appendChild(ingitem);
     })
+//creating element for steps section
+    const stepSection = document.createElement('h2');
+    stepSection.classList.add('recipeLibraryIngredients');
+    stepSection.innerHTML = 'Steps';
 
-    //creating element for steps step
+//creating element for steps step
     const steps = document.createElement('ol');
     steps.classList.add('recipeLibrarySteps')
-    //for loop for item list
+    ingredientsList.classList.add('content');
+    ingredientsList.classList.add('box');
+
+//for loop for item list
     recipe.steps.forEach((step, index) => {
     const stepsItem = document.createElement('li');
     stepsItem.innerHTML = step;
     steps.appendChild(stepsItem);
     })
 
-    // appending to divitem 
-
+// appending to divitem 
+    
     divItem.appendChild(recipeName);
     divItem.appendChild(ingSection);
     divItem.appendChild(ingredientsList);
+    divItem.appendChild(stepSection);
     divItem.appendChild(steps);
 
-    //appending divItem to recipeList
+//appending divItem to recipeList
     recipesList.appendChild(divItem);
 })
